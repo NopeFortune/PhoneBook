042d771eb98f27b15a6da1d54b2b7a31fcf9ca45
NopeFortune <nuclearindustry@gmail.com>
2021-08-02 19:23:20 +0300

    Убраны лишние using'и

--- PhoneBook/PhoneBook/Form1.Designer.cs
+++ PhoneBook/PhoneBook/Form1.Designer.cs
@@ -258,6 +258,7 @@ namespace PhoneBook
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
--- PhoneBook/PhoneBook/Form1.cs
+++ PhoneBook/PhoneBook/Form1.cs
@@ -7,7 +7,7 @@ namespace PhoneBook
 {
     public partial class Form1 : Form
     {
-        string ConnectionString = "Server=(localdb)\\MSSQLLocalDB;Initial Catalog=PhoneBook;Integrated Security=True;";
+        const string CONNECTION_STRING = "Server=(localdb)\\MSSQLLocalDB;Initial Catalog=PhoneBook;Integrated Security=True;";
         int Id = 0;
         public Form1()
         {
@@ -16,30 +16,25 @@ namespace PhoneBook
             btnDelete.Enabled = false;
         }
 
+        SqlConnection connection = new SqlConnection(CONNECTION_STRING);
+        
         /// <summary>
         /// Метод для отображения данных из базы данных в DataGridView
         /// </summary>
         public void ShowData()
         {
-
-            using (SqlConnection connection = new SqlConnection(ConnectionString))
-            {
-                connection.Open();
                 SqlDataAdapter adapter = new SqlDataAdapter("sp_SelectAllContacts", connection);
                 adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
                 DataTable dataTable = new DataTable();
                 adapter.Fill(dataTable);
                 dgvData.DataSource = dataTable;
-            }
+                
         }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
             if (tbFirstName.Text != "" && tbPhone.Text != "")
             {
-                using (SqlConnection connection = new SqlConnection(ConnectionString))
-                {
-                    connection.Open();
                     SqlCommand command = new SqlCommand("sp_AddOrUpdateContact", connection);
                     command.CommandType = CommandType.StoredProcedure;
                     command.Parameters.AddWithValue("@Id", Id);
@@ -49,7 +44,6 @@ namespace PhoneBook
                     command.Parameters.AddWithValue("@Email", tbEmail.Text.Trim());
                     command.Parameters.AddWithValue("@Adress", tbAdress.Text.Trim());
                     command.ExecuteNonQuery();
-                }
             }
             else
             {
@@ -61,15 +55,11 @@ namespace PhoneBook
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
-            using (SqlConnection connection = new SqlConnection(ConnectionString))
-            {
-                connection.Open();
-
                 SqlCommand command = new SqlCommand("sp_DeleteContactById", connection);
                 command.CommandType = CommandType.StoredProcedure;
                 command.Parameters.AddWithValue("@Id", Id);
                 command.ExecuteNonQuery();
-            }
+
             ShowData();
             btnClear_Click(sender, e);
         }
@@ -115,8 +105,6 @@ namespace PhoneBook
         /// <param name="e"></param>
         private void tbSearch_TextChanged(object sender, EventArgs e)
         {
-            using (SqlConnection connection = new SqlConnection(ConnectionString))
-            {
                 connection.Open();
                 SqlDataAdapter adapter = new SqlDataAdapter("sp_Search", connection);
                 adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
@@ -124,7 +112,6 @@ namespace PhoneBook
                 DataTable dataTable = new DataTable();
                 adapter.Fill(dataTable);
                 dgvData.DataSource = dataTable;
-            }
         }
 
         private void tbFirstName_TextChanged(object sender, EventArgs e)
@@ -167,6 +154,7 @@ namespace PhoneBook
                 DialogResult dialogResult = MessageBox.Show("Do you want to exit without saving your data?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
                 if (dialogResult == DialogResult.Yes)
                 {
+                    connection.Close();
                     Application.ExitThread();
                 }
                 else if (dialogResult == DialogResult.No)
@@ -174,6 +162,11 @@ namespace PhoneBook
                     e.Cancel = true;
                 }
             }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connection.Open();
         }
     }
 }
